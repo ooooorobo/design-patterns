9f74fe258a22b9f4af6facdd39948be16fb9213e
조예진 <yjcho@miridih.com>
2023-06-06 17:59:37 +0900

    feat: 표 형식으로 목차 생성

--- .github/toc-maker.js
+++ .github/toc-maker.js
@@ -3,6 +3,24 @@ const path = require('path')
 
 const types = ['behavioral', 'creational', 'structural']
 
+const authorToId = {
+    '조예진': 'ooooorobo',
+    'Paul An': 'anpaul0615',
+    'paulan': 'anpaul0615',
+    'pshdev1030': 'pshdev1030',
+}
+const authorToName = {
+    '조예진': '조예진',
+    'Paul An': '안바울',
+    'paulan': '안바울',
+    'pshdev1030': '박성현',
+}
+const typeToName = {
+    'behavioral': '행동 패턴',
+    'creational': '생성 패턴',
+    'structural': '구조 패턴',
+}
+
 const readFromDirectory = async (dirPath) => {
     return new Promise((resolve, reject) => {
         fs.readdir(dirPath, (err, files) => {
@@ -61,15 +79,40 @@ const replaceWriting = (fileContents, toReplaced) => {
     return fileContents.replace(/(<!-- toc starts -->)(.|\n)*(<!-- toc ends -->)/, toReplaced)
 }
 
-(async () => {
-    const paths = (await Promise.all(types.map(type => getAllFilePath(type))))
-        .map((pathsInType, idx) => pathsInType
-            .filter(x => x.includes('.md'))
-            .map(x => `[${x.slice(x.lastIndexOf('/') + 1, x.indexOf('.'))}](./${types[idx]}${x})`)
-        )
+const parseFrontMatter = (contents) => {
+    const meta = {}
+    const lines = contents.match(/---([\s\S]*?)---/)?.[0].split('\n')
+    for (const line of lines) {
+        if (line.startsWith('---')) continue;
+        const colonPos = line.indexOf(':');
+        const key = line.slice(0, colonPos), value = line.slice(colonPos + 1).trim()
+        meta[key] = key === 'date' ? new Date(value) : value;
+    }
+    return meta
+}
 
-    // TODO: readme 내에서 제목 가져오기
-    const toc = types.map((type, idx) => `### ${type}\n\n- ${paths[idx].join('\n- ')}`).join('\n\n')
+
+(async () => {
+    const metaList = [];
+
+    for (const type of types) {
+        const paths = (await getAllFilePath(type)).filter(x => x.includes('.md'));
+        for (const pathName of paths) {
+            const contents = (await readFile(`${type}${pathName}`)).toString()
+            metaList.push({...parseFrontMatter(contents), path: `./${type}${pathName}`});
+        }
+    }
+
+    const header = '| Title | Author | Date |\n| ----------- | ----- | ---- |\n'
+    const toc = types.map(type =>
+        `\n\n### ${typeToName[type]}\n` +
+        header +
+        metaList
+            .filter(x => x.type === type)
+            .sort((a, b) => a.date - b.date)
+            .map(x => `|[${x.title}](${x.path})|[${authorToName[x.author]}](https://github.com/${authorToId[x.author]})|${x.date.toLocaleDateString('ko')}|`)
+            .join('\n')
+    );
 
     const readme = (await readFile(path.join(process.cwd(), 'README_TEMPLATE.md'))).toString()
     await writeFile(path.join(process.cwd(), 'README.md'), replaceWriting(readme, toc))
